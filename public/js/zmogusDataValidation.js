fabf242763871d8f0d3d047f7a6dae9da9843d1d
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-10-04 11:00:45 +0300

    Data validation

---
+++ public/js/zmogusDataValidation.js
@@ -0,0 +1,19 @@
+// *****************************************************************************
+// DATA VALIDATION IMPORT
+import {hasSpecChar} from './dataValidation.js'
+// *****************************************************************************
+
+
+        const vardas = document.getElementById('vardas').nodeValue;
+        // const pavarde = req.body.pavarde;
+        // const gimData = new Date(req.body.gimData);
+        // const alga = parseFloat(req.body.alga);
+        // if (typeof vardas !== 'string' || vardas.trim() === '' || hasSpecChar(vardas)) {
+        //     throw 'Vardas negali būti tuščias arba turėti spec simbolių ar skaičių!';
+        // } else if (typeof pavarde !== 'string' || pavarde.trim()  == '' || hasSpecChar(pavarde)) {
+        //     throw 'Pavardė negali būti tuščia arba turėti spec simbolių ar skaičių!';
+        // } else if (!isFinite((gimData).getTime())) {
+        //     throw 'Blogai nurodyta gimimo data!';
+        // } else if(isFinite(alga)) 
+
+        console.log(vardas);
